b965549693d68c67801aefed6fe5fbede6739906
Matt Flax <flatmax@flatmax.org>
2020-12-29 13:39:51 +1100

    Update the kernel's bootargs, requires more work to get console printing

--- board/RK3308/boot.cmd
+++ board/RK3308/boot.cmd
@@ -1,3 +1,4 @@
+setenv bootargs "root=/dev/mmcblk1 earlyprintk=serial,uart0,115200 rw rootwait"
 fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtfile}
 fatload mmc ${devnum}:${distro_bootpart} ${kernel_addr_c} Image
 booti ${kernel_addr_c} - ${fdt_addr_r}
