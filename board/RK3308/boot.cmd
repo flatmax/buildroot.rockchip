baa1f997107038fa5072749100da10976fb98449
Matt Flax <flatmax@flatmax.org>
2021-01-01 13:56:29 +1100

    System now boots to login prompt after updating the rootfs part num.

--- board/RK3308/boot.cmd
+++ board/RK3308/boot.cmd
@@ -1,5 +1,5 @@
-setenv verbosity "7"
-setenv bootargs "root=/dev/mmcblk0p1 earlyprintk console=ttyS0,115200n8 rw rootwait"
+
+setenv bootargs "root=/dev/mmcblk0p2 earlyprintk console=ttyS0,115200n8 rw rootwait"
 fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtfile}
 fatload mmc ${devnum}:${distro_bootpart} ${kernel_addr_r} Image
 booti ${kernel_addr_r} - ${fdt_addr_r}
