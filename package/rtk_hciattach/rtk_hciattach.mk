b85950c7cc5619d36565d61c8032acb6fa6bd94d
Matt Flax <flatmax@flatmax.org>
2021-05-12 17:06:26 +1000

    Install the rtk_hciattach firmware.

--- package/rtk_hciattach/rtk_hciattach.mk
+++ package/rtk_hciattach/rtk_hciattach.mk
@@ -9,7 +9,9 @@ RTK_HCIATTACH_LICENSE = GPLv2
 RTK_HCIATTACH_INSTALL_TARGET = YES
 
 define RTK_HCIATTACH_INSTALL_TARGET_CMDS
-   $(INSTALL) -D -m 0755 $(@D)/realtek/rtk_hciattach/rtk_hciattach $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/realtek/rtk_hciattach/rtk_hciattach $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0600 $(@D)/realtek/RTL8723DS/rtl8723d_fw $(TARGET_DIR)/lib/firmware/rtlbt
+	$(INSTALL) -D -m 0600 $(@D)/realtek/RTL8723DS/rtl8723d_config $(TARGET_DIR)/lib/firmware/rtlbt
 endef
 
 define RTK_HCIATTACH_BUILD_CMDS
