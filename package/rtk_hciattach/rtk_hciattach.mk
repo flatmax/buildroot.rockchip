21c0fca46a970405072a613b473848b3c3c0ff5f
Matt Flax <flatmax@flatmax.org>
2021-05-12 16:58:43 +1000

    using rxdata for bluetooth rtk_hciattach.mk

--- package/rtk_hciattach/rtk_hciattach.mk
+++ package/rtk_hciattach/rtk_hciattach.mk
@@ -4,10 +4,7 @@
 #
 ################################################################################
 RTK_HCIATTACH_VERSION = HEAD
-#RTK_HCIATTACH_SITE = $(call github,flatmax,rtk_hciattach,$(RTK_HCIATTACH_VERSION))
-RTK_HCIATTACH_SITE = $(call github,rockchip-linux,rkwifibt,$(RTK_HCIATTACH_VERSION))
-# RTK_HCIATTACH_SITE = /home/flatmax/temp/rtk_hciattach
-# RTK_HCIATTACH_SITE_METHOD = local
+RTK_HCIATTACH_SITE = $(call github,radxa,rkwifibt,$(RTK_HCIATTACH_VERSION))
 RTK_HCIATTACH_LICENSE = GPLv2
 RTK_HCIATTACH_INSTALL_TARGET = YES
 
