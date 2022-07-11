1eb82235fd764b2d6bad2a797ae35b97ca2f745f
Matt Flax <flatmax@flatmax.com>
2022-07-11 11:21:49 +1000

    Use host tools for mkimage and uboot boot.scr

--- board/RK3566.cm3/post-image.sh
+++ board/RK3566.cm3/post-image.sh
@@ -17,9 +17,7 @@ fi
 ubootName=`find $BASE_DIR/build -name 'uboot-*' -type d`
 boardDir=`dirname $_`
 
-#echo creating uboot.img
-#cp $ubootName/u-boot.img $BINARIES_DIR/uboot.img
-#$RKBIN/tools/loaderimage --pack --uboot $ubootName/u-boot-dtb.bin $BINARIES_DIR/uboot.img
+echo creating uboot.img
 currentDir=`pwd`
 cd $ubootName; ./make.sh;
 cd $currentDir
@@ -28,15 +26,10 @@ cp $ubootName/uboot.img $BINARIES_DIR/u-boot.itb
 # to take rockchip-bsp's boot loaders, rather then generating our own ...
 echo creating idbloader.img
 $ubootName/tools/mkimage -n rk3568 -T rksd -d $RKBIN/bin/rk35/rk3566_ddr_1056MHz_v1.08.bin:$ubootName/spl/u-boot-spl.bin $BINARIES_DIR/idbloader.img
-#$ubootName/tools/mkimage -n rk3568 -T rksd -d $RKBIN/bin/rk35/rk3566_ddr_1056MHz_v1.08.bin $BINARIES_DIR/idbloader.img
-#cat ${RKBIN}/bin/rk35/rk3568_bl31_v1.24.elf >> $BINARIES_DIR/idbloader.img
-#${boardDir}/mkRK3566Boot.sh ${BINARIES_DIR} ${RKBIN} rk356x_spl_loader_v1.08.111.bin
-#cp $BINARIES_DIR/rk356x_spl_loader_v1.08.111.bin $BINARIES_DIR/idbloader.img
-#cp $ubootName/rk356x_spl_loader_v1.08.111.bin $BINARIES_DIR/idbloader.img
 
 # Generate the uboot script
 echo creating boot.scr
-$ubootName/tools/mkimage -C none -A arm -T script -d $BR2_EXTERNAL_RK3308_PATH/board/RK3566.cm3/boot.cmd $BINARIES_DIR/boot.scr
+$HOST_DIR/bin/mkimage -C none -A arm -T script -a 0x00c00000 -e 0x00c00000 -n 'flatmax load script' -d $BR2_EXTERNAL_RK3308_PATH/board/RK3566.cm3/boot.cmd $BINARIES_DIR/boot.scr
 
 #make the trust image
 echo creating trust.img
@@ -57,12 +50,3 @@ echo use the following command ...
 echo
 echo 'OF=/dev/sdb; rootDrive=`mount | grep " / " | grep $OF`; if [ -z $rootDrive ]; then sudo umount $OF[123456789]; sudo dd if=output/images/sdcard.img of=$OF bs=10M; else echo you are trying to overwrite your root drive; fi'
 echo
-# echo
-# echo
-# echo
-# echo NOTE: Uboot booting is currently a manual process. Upon boot when you are stopped in uboot in the console, execute the following :
-# echo
-# echo setenv devnum 1
-# echo
-# echo now copy and paste the contents of RK3566.cm3/boot.cmd into the uboot console. Linux will boot. Login as appropriate
-# echo
