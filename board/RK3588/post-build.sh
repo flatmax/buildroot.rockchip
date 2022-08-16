076ba4da6db3016e171de3f0189e78492ecda25e
Matt Flax <flatmax@flatmax.com>
2022-08-17 07:43:51 +1000

    rk3588 add module autoload support at boot time.

--- board/RK3588/post-build.sh
+++ board/RK3588/post-build.sh
@@ -7,3 +7,11 @@ if [ $cnt -eq "0" ]; then
   echo "auto eth0">> $TARGET_DIR/etc/network/interfaces
   echo "iface eth0 inet dhcp">> $TARGET_DIR/etc/network/interfaces
 fi
+
+# modules
+if [ ! -f $TARGET_DIR/etc/init.d/S02modules ]; then
+  cp $BR2_EXTERNAL_RK3308_PATH/board/RK3588/S02modules $TARGET_DIR/etc/init.d
+fi
+if [ ! -f $TARGET_DIR/etc/modules ]; then
+  echo 'r8125.ko' > $TARGET_DIR/etc/modules
+fi
