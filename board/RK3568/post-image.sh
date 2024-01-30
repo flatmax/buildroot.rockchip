8e823a046fa366facb7d2047f9293f1822af95a4
haelyons <helios.lyons@disguise.one>
2024-01-30 12:19:26 +0000

    Update post-image Fix issue with manual intervention for fastboot buf addr Add overlays to board/RK3568

--- board/RK3568/post-image.sh
+++ board/RK3568/post-image.sh
@@ -4,28 +4,24 @@ RKBIN=$BINARIES_DIR/rkbin
 RKCHIP_LOADER=$2
 RKCHIP=$2
 
+UBOOT_DIR=`find $BASE_DIR/build -name 'uboot-*' -type d | head -n 1`
+LINUX_DIR=`find $BASE_DIR/build -name 'vmlinux' -type f | xargs dirname`
+
 # copy uboot variable file over
 cp -a $BR2_EXTERNAL_RK3308_PATH/board/RK3568/vars.txt $BINARIES_DIR/
 
-# copy overlays over
-linuxDir=`find $BASE_DIR/build -name 'vmlinux' -type f | xargs dirname`
+# overlays are no longer compiled in kernel as dtbo, add to overlays dir and copy to binaries dir
 mkdir -p $BINARIES_DIR/rockchip/overlays
-if [ -d ${linuxDir}/arch/arm64/boot/dts/rockchip/overlay ]; then
-  cp -a ${linuxDir}/arch/arm64/boot/dts/rockchip/overlay/*.dtbo $BINARIES_DIR/rockchip/overlays
-fi
-
-ubootName=`find $BASE_DIR/build -name 'uboot-*' -type d | head -n 1`
-boardDir=`dirname $_`
+cp -a $BR2_EXTERNAL_RK3308_PATH/board/RK3568/overlays/* $BINARIES_DIR/rockchip/overlays/
+cp $LINUX_DIR/arch/arm64/boot/dts/rockchip/rk3568-rock-3a.dtb $BINARIES_DIR/rockchip/rk3568-rock-3a.dtb
 
 echo copying uboot.itb
-echo $ubootName
-echo $BINARIES_DIR
-cp $ubootName/u-boot.itb $BINARIES_DIR/u-boot.itb
+cp $UBOOT_DIR/u-boot.itb $BINARIES_DIR/u-boot.itb
 
 # uboot creation
 # to take rockchip-bsp's boot loaders, rather then generating our own ...
 #cp ~/temp/rockchip-bsp/out/u-boot/idbloader.img ~/temp/rockchip-bsp/out/u-boot/u-boot.itb $BINARIES_DIR/
-$ubootName/tools/mkimage -n rk3568 -T rksd -d $RKBIN/bin/rk35/rk3568_ddr_1056MHz_v1.08.bin:$ubootName/spl/u-boot-spl.bin $BINARIES_DIR/idbloader.img
+$UBOOT_DIR/tools/mkimage -n rk3568 -T rksd -d $RKBIN/bin/rk35/rk3568_ddr_1056MHz_v1.08.bin:$UBOOT_DIR/spl/u-boot-spl.bin $BINARIES_DIR/idbloader.img
 
 # Generate the uboot script
 $HOST_DIR/bin/mkimage -C none -A arm -T script -d $BR2_EXTERNAL_RK3308_PATH/board/RK3568/boot.cmd $BINARIES_DIR/boot.scr
