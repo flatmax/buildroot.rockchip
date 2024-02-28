4b10bd360c9efbe0cc51b544136d8abed871872c
Matt Flax <flatmax@flatmax.com>
2024-02-28 14:21:19 +1100

    switch to latest rkbin version

--- package/rkbin/rkbin.mk
+++ package/rkbin/rkbin.mk
@@ -4,17 +4,14 @@
 #
 ################################################################################
 
-RKBIN_VERSION = 7d631e0d5b2d373b54d4533580d08fb9bd2eaad4
-RKBIN_SITE = https://github.com/rockchip-linux/rkbin.git
+RKBIN_VERSION = fc44f9401c127affb2a879c1e90fa89ddab505f6
+RKBIN_SITE = git@github.com:radxa/rkbin.git
 RKBIN_SITE_METHOD = git
 RKBIN_INSTALL_IMAGES = YES
 
 define RKBIN_INSTALL_IMAGES_CMDS
 	mkdir -p $(BINARIES_DIR)/rkbin
 	cp -a $(@D)/* $(BINARIES_DIR)/rkbin
-#	cp $(@D)/bin/rk33/rk3308_ddr_589MHz_uart2_m1_v1.30.bin $(BINARIES_DIR)
-#	cp $(@D)/bin/rk33/rk3308_miniloader_v1.19.bin $(BINARIES_DIR)
-#	cp $(@D)/bin/rk33/rk3308_bl31_v2.22.elf $(BINARIES_DIR)
 endef
 
 $(eval $(generic-package))
