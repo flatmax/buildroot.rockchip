d9746646ec82b52d15c84d91902cd35c3828a86f
Matt Flax <flatmax@flatmax.com>
2025-02-11 20:57:55 +1100

    cleanup aes67-setup

--- package/aes67-setup/aes67-setup.mk
+++ package/aes67-setup/aes67-setup.mk
@@ -1,11 +1,10 @@
 ################################################################################
 #
-# azes67-setup
+# aes67-setup
 #
 ################################################################################
 
 AES67_SETUP_VERSION = 0.1.0
-#AES67_SETUP_DEPENDENCIES = alsa-utils ethtool pipewire linuxptp
 
 AES67_SETUP_VERSION = 0.0.1
 AES67_SETUP_SITE = $(BR2_EXTERNAL)/package/aes67-setup
@@ -16,11 +15,5 @@ $(INSTALL) -m 0755 -D $(AES67_SETUP_PKGDIR)/S95-aes67 $(TARGET_DIR)/etc/init.d/
 $(INSTALL) -m 0755 -D $(AES67_SETUP_PKGDIR)/S96-wireplumber $(TARGET_DIR)/etc/init.d/
 endef
 AES67_SETUP_POST_INSTALL_TARGET_HOOKS += AES67_SETUP_INSTALL_INIT_SYSV
-
-# define AES67_SETUP_INSTALL_MORE
-# # $(INSTALL) -m 0755 -D $(@D)/class/FileWatcher.js $(TARGET_DIR)/usr/share/DeqxBox/class/FileWatcher.js
-# # $(RM) -rf $(TARGET_DIR)/usr/share/doc/deqxbox
-# endef
-# AES67_SETUP_POST_INSTALL_TARGET_HOOKS += AES67_SETUP_INSTALL_MORE
 
 $(eval $(generic-package))
--- package/aes67-setup/ptp-startup.sh
+++
@@ -1,1 +0,0 @@
-ptp4l -i eth0 -f /etc/ptp4l_gptp.conf
